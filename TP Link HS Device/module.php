bec3151977ba533b3adc93f956db7a56f5894eca
Fonzo <aggadur@gmail.com>
2018-05-20 18:52:35 +0200

    add profiles for TP Link HS

--- TP Link HS Device/module.php
+++ TP Link HS Device/module.php
@@ -35,15 +35,22 @@
 		{
 			//Never delete this line!
 			parent::ApplyChanges();
-            $this->RegisterVariableBoolean("State", "Status", "~Switch", 1);
+
+			$this->RegisterVariableBoolean("State", "Status", "~Switch", 1);
             $this->EnableAction("State");
 			$model = $this->ReadPropertyInteger("modelselection");
 			if($model == 2)
             {
-                $this->RegisterVariableFloat("Voltage", "Spannung", "~Volt.230", 2);
-                $this->RegisterVariableFloat("Power", "Leistung", "~Watt.14490", 3);
-                $this->RegisterVariableFloat("Current", "Strom", "~Milliampere.HM", 4);
-                $this->RegisterVariableFloat("Work", "Arbeit", "~Electricity.HM", 5);
+				$this->RegisterProfile('TPLinkHS.Volt.230', 'Electricity', '', " V", 207, 253, 0, 1, 2);
+				$this->RegisterProfile('TPLinkHS.Watt.14490', 'Electricity', '', " W", 0, 14490, 0, 1, 2);
+				$this->RegisterProfile('TPLinkHS.Milliampere', '', '', " mA", 0, 0, 0, 0, 2);
+				$this->RegisterProfile('TPLinkHS.Electricity', '', '', " Wh", 0, 0, 0, 2, 2);
+
+
+                $this->RegisterVariableFloat("Voltage", "Spannung", "TPLinkHS.Volt.230", 2);
+                $this->RegisterVariableFloat("Power", "Leistung", "TPLinkHS.Watt.14490", 3);
+                $this->RegisterVariableFloat("Current", "Strom", "TPLinkHS.Milliampere", 4);
+                $this->RegisterVariableFloat("Work", "Arbeit", "TPLinkHS.Electricity", 5);
             }
 			$this->ValidateConfiguration();	
 		}
@@ -848,46 +855,79 @@
 
 		
 		//Profile
+		/**
+		 * register profiles
+		 * @param $Name
+		 * @param $Icon
+		 * @param $Prefix
+		 * @param $Suffix
+		 * @param $MinValue
+		 * @param $MaxValue
+		 * @param $StepSize
+		 * @param $Digits
+		 * @param $Vartype
+		 */
 		protected function RegisterProfile($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits, $Vartype)
 		{
-			
-			if(!IPS_VariableProfileExists($Name))
-			{
+
+			if (!IPS_VariableProfileExists($Name)) {
 				IPS_CreateVariableProfile($Name, $Vartype); // 0 boolean, 1 int, 2 float, 3 string,
-			}
-			else
-			{
+			} else {
 				$profile = IPS_GetVariableProfile($Name);
-				if($profile['ProfileType'] != $Vartype)
-				throw new Exception("Variable profile type does not match for profile ".$Name);
+				if ($profile['ProfileType'] != $Vartype) {
+					$this->_debug('profile', 'Variable profile type does not match for profile ' . $Name);
+				}
 			}
-			
+
 			IPS_SetVariableProfileIcon($Name, $Icon);
 			IPS_SetVariableProfileText($Name, $Prefix, $Suffix);
 			IPS_SetVariableProfileDigits($Name, $Digits); //  Nachkommastellen
 			IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize); // string $ProfilName, float $Minimalwert, float $Maximalwert, float $Schrittweite
 		}
-		
+
+		/**
+		 * register profile association
+		 * @param $Name
+		 * @param $Icon
+		 * @param $Prefix
+		 * @param $Suffix
+		 * @param $MinValue
+		 * @param $MaxValue
+		 * @param $Stepsize
+		 * @param $Digits
+		 * @param $Vartype
+		 * @param $Associations
+		 */
 		protected function RegisterProfileAssociation($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $Stepsize, $Digits, $Vartype, $Associations)
 		{
-			if ( sizeof($Associations) === 0 ){
+			if (is_array($Associations) && sizeof($Associations) === 0) {
 				$MinValue = 0;
 				$MaxValue = 0;
-			} 
-			/*
-			else {
-				//undefiened offset
-				$MinValue = $Associations[0][0];
-				$MaxValue = $Associations[sizeof($Associations)-1][0];
 			}
-			*/
 			$this->RegisterProfile($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $Stepsize, $Digits, $Vartype);
-			
-			//boolean IPS_SetVariableProfileAssociation ( string $ProfilName, float $Wert, string $Name, string $Icon, integer $Farbe )
-			foreach($Associations as $Association) {
-				IPS_SetVariableProfileAssociation($Name, $Association[0], $Association[1], $Association[2], $Association[3]);
+
+			if (is_array($Associations)) {
+				foreach ($Associations AS $Association) {
+					IPS_SetVariableProfileAssociation($Name, $Association[0], $Association[1], $Association[2], $Association[3]);
+				}
+			} else {
+				$Associations = $this->$Associations;
+				foreach ($Associations AS $code => $association) {
+					IPS_SetVariableProfileAssociation($Name, $code, $this->Translate($association), $Icon, -1);
+				}
 			}
-			
+
+		}
+
+		/**
+		 * send debug log
+		 * @param string $notification
+		 * @param string $message
+		 * @param int $format 0 = Text, 1 = Hex
+		 */
+		private function _debug(string $notification = NULL, string $message = NULL, $format = 0)
+		{
+			$this->SendDebug($notification, $message, $format);
 		}
 
         //Configuration Form
